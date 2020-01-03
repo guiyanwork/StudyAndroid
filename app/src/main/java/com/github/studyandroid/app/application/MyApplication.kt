17de6a8e516e2b9ce97ece34ded4399640f0ceea
Gui Yan <guiyanwork@163.com>
2020-01-03 17:45:29 +0800

    android app finish

--- app/src/main/java/com/github/studyandroid/app/application/MyApplication.kt
+++ app/src/main/java/com/github/studyandroid/app/application/MyApplication.kt
@@ -1,8 +1,6 @@
 package com.github.studyandroid.app.application
 
 import android.app.Application
-import android.content.Context
-import androidx.multidex.MultiDex
 
 /**
  * Copyright (c) 2019 GitHub, Inc.
@@ -10,11 +8,6 @@ import androidx.multidex.MultiDex
  * Author(s): Gui Yan (guiyanwork@163.com)
  */
 class MyApplication : Application() {
-    override fun attachBaseContext(base: Context?) {
-        super.attachBaseContext(base)
-        MultiDex.install(this)
-    }
-
     override fun onCreate() {
         super.onCreate()
         CrashHandler.instance
